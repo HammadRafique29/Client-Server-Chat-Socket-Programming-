5838cb5d717b06258a62b88db48f6442715f8b49
Hammad Rafique <112252001+HammadRafique29@users.noreply.github.com>
2023-06-24 10:30:04 +0500

    Initial Commit

---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+
+# Connection Data
+host = '127.0.0.1'   #LOCAL host
+port = 55555     # not a reserved host
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:         # try to recieve a message from the client
+            # if recieved then Broadcast the Message to all clients
+            message = client.recv(1024)
+            broadcast(message)
+        except: # if an error has occured
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listening.....")
+receive()
